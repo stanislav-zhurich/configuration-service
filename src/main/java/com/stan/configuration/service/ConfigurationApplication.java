440780eb5cfce7cfd3641163dc4a0edf42f8f57b
Zhurich, Stanislav <Stanislav.Zhurich@Kronos.com>
2017-01-08 13:43:46 +0300

    Add Dockerfile

--- src/main/java/com/stan/configuration/service/ConfigurationApplication.java
+++ src/main/java/com/stan/configuration/service/ConfigurationApplication.java
@@ -1,4 +1,4 @@
-package com.stan;
+package com.stan.configuration.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
